d8cfe6c889d3771d0f5baf958156998d45fac0e4
mjolsen-sw <mjolsen.sw@gmail.com>
2025-07-02 06:13:01 -0700

    clean up

--- aws/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "mjolsen-mern-deployment-terraform-state"
-    key            = "mern-deployment/terraform.tfstate"
-    region         = "us-west-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- aws/terraform/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-aws_region             = "us-west-1"
-environment            = "dev"
-vpc_cidr_block         = "10.0.0.0/16"
-acm_certificate_arn    = "arn:aws:acm:us-west-1:009160064216:certificate/60bfbcf1-2020-46b1-b19d-ab82eab34eb0"
